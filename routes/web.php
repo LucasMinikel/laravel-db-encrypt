ca1f10ab5447e373c5b8782aa2ab63e77917fca4
LucasMinikel <lucasminikel.m@gmail.com>
2024-10-02 12:07:27 -0300

    teste padrao

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    $user = User::find(1);
+    $users = User::where('cpf', Crypt::encrypt('12345678909'))->get();
+    dd($user->cpf, $users);
     return view('welcome');
 });
