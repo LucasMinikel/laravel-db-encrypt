9dac5e4fc8c1ccde4930e7512a5bdae805eee995
LucasMinikel <lucasminikel.m@gmail.com>
2024-10-02 12:06:48 -0300

    adicionando cpf user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => fake()->name,
+            'email' => fake()->email,
+            'cpf' => '12345678909',
         ]);
     }
 }
